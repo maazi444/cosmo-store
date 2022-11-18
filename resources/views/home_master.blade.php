5a5149f9bf7d70879e650095bbec6b766f5264ff
M Maaz Ahmad <maazahmad915@gmail.com>
2022-11-18 15:15:42 +0500

    home_master file added

---
+++ resources/views/home_master.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+
+    <meta property="og:title" content="">
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
+    <title>Home Page</title>
+</head>
+
+<body>
+    <main>
+        <h1>Hello world</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto, nulla amet doloribus autem vero, ad reprehenderit cupiditate numquam est voluptatem modi voluptate, laborum non soluta? Numquam error enim doloribus corporis.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe modi vel quidem asperiores animi doloremque possimus odio molestias tenetur neque provident quas, voluptates cum similique nihil perspiciatis quia eum eos.</p>
+        <h3>This is h3 heading</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt laborum temporibus fugiat natus, doloremque magni enim repellat voluptas deserunt iste dolorem cumque provident rem corrupti facere eius ut tempore optio.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quidem reprehenderit iure eos cumque? Accusantium accusamus aperiam enim, dolores aut sit fuga ipsam voluptatum modi. Id aspernatur impedit mollitia minus.</p>
+    </main>
+</body>
+
+</html>
