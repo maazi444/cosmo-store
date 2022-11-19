b0787825fe6458139d32bc0ac9d38be2ef2f8835
M Maaz Ahmad <maazahmad915@gmail.com>
2022-11-19 14:44:03 +0500

    featured product section added

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -16,6 +16,54 @@
     </div>
 </div>
 <section class="homeCategory">
-
+    <div class="homeCategory__images">
+        <div class="homeCategory__col1">
+            <!-- Featured Product Start -->
+            <div class="homeCategory__item">
+                <div class="homeCategory__itemInner">
+                    <a href="#" class="homeCategory__link"></a>
+                    <div class="homeCategory__thumbnail">
+                        <img src="{{ asset('frontend/img/product28_large.webp') }}" class="homeCategory__overlay" alt="">
+                    </div>
+                </div>
+            </div>
+            <!-- Featured Product End -->
+            <!-- Featured Product Start -->
+            <div class="homeCategory__item">
+                <div class="homeCategory__itemInner">
+                    <a href="#" class="homeCategory__link"></a>
+                    <div class="homeCategory__thumbnail">
+                        <img src="{{ asset('frontend/img/product9_large.webp') }}" class="homeCategory__overlay" alt="">
+                    </div>
+                </div>
+            </div>
+            <!-- Featured Product End -->
+        </div>
+        <div class="homeCategory__col2">
+            <!-- Featured Product Start -->
+            <div class="homeCategory__item">
+                <div class="homeCategory__itemInner">
+                    <a href="#" class="homeCategory__link"></a>
+                    <div class="homeCategory__thumbnail">
+                        <img src="{{ asset('frontend/img/product29_large.webp') }}" class="homeCategory__overlay" alt="">
+                    </div>
+                </div>
+            </div>
+            <!-- Featured Product End -->
+            <!-- Featured Product Start -->
+            <div class="homeCategory__item">
+                <div class="homeCategory__itemInner">
+                    <a href="#" class="homeCategory__link"></a>
+                    <div class="homeCategory__thumbnail">
+                        <img src="{{ asset('frontend/img/product31_large.webp') }}" class="homeCategory__overlay" alt="">
+                    </div>
+                </div>
+            </div>
+            <!-- Featured Product End -->
+        </div>
+    </div>
+    <div class="homeCategory__text">
+        Home Text
+    </div>
 </section>
 @endsection
