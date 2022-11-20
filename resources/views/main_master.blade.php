f8d91c6cf652e71a4ddf13156f87a80a8f48c37a
M Maaz Ahmad <maazahmad915@gmail.com>
2022-11-20 09:18:06 +0500

    cta and founder section added

--- resources/views/main_master.blade.php
+++ resources/views/main_master.blade.php
@@ -12,9 +12,17 @@
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.cdnfonts.com/css/hk-groteks" rel="stylesheet" />
     <!-- font-family: 'HK Grotesk', sans-serif; -->
 
+    <!--FONT AWESOME-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!--GOOGLE FONTS-->
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Play&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css">
     <link rel="stylesheet" href="{{ asset('frontend/css/normalize.css') }}" />
     <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}" />
     <link rel="stylesheet" href="{{ asset('frontend/css/main.css') }}" />
@@ -25,6 +33,8 @@
     @include('body.header')
 
     @yield('main')
+
+    @include('body.footer')
 
     <script src="{{
                 asset('frontend/js/vendor/modernizr-3.11.2.min.js')
